397a787865953b3a150bda1969118a7d172c07a3
Noronha <carlos.noronha.93@gmail.comm>
2020-10-14 15:27:50 +0100

    added navbar and navTrigger

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ console.log(cta);
 setTimeout(() => {
     cta.style = 'visibility: visible;'
 }, 3500);
+
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+});
+
+$('.navlinks').children().children().click(() => {
+    $('#mainListDiv').removeClass('show_list');
+})
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+        $('.nav').addClass('affix');
+    } else {
+        $('.nav').removeClass('affix');
+    }
+});
