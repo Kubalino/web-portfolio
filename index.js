69cf7073981b063cb8667caaec1485d397840c3f
Noronha <carlos.noronha.93@gmail.comm>
2020-10-18 19:49:50 +0100

    added light and dark theme, final adjustments to media screens

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const cta = document.querySelector('.cta');
 
-console.log(cta);
+let mode = localStorage.getItem('theme');
+
+if(mode == null) {
+    setTheme('light');
+} else {
+    setTheme(mode);
+}
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for(var i = 0; themeDots.length > i; i++) {
+    themeDots[i].addEventListener('click', function() {
+        let mode = this.dataset.mode
+        console.log('option clicked', mode);
+        setTheme(mode);    
+    })
+}
+
+function setTheme (mode) {
+    if(mode == 'light') {
+        document.querySelector('#theme-style').href = 'light.css';
+        document.querySelector('.picture').innerHTML = `<img id="profile-pic" src="assets/blue_bg.png">`;
+    }
+
+    if (mode == 'dark') {
+        document.querySelector('#theme-style').href = 'dark.css';
+        document.querySelector('.picture').innerHTML = `<img id="profile-pic" src="assets/red_bg.jpg">`;
+    }
+
+    localStorage.setItem('theme', mode);
+}
 
 setTimeout(() => {
     cta.style = 'visibility: visible;'
@@ -23,3 +53,14 @@ $(window).scroll(function() {
         $('.nav').removeClass('affix');
     }
 });
+
+
+
+
+
+
+
+
+
+
+    
